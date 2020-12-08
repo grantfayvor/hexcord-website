ae5fad0cd358884569b0c82d75be6bdca9dbcace
unknown <sadiqajimustapha@gmail.com>
2020-12-08 15:52:11 -0800

    Added Privacy policy page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import './App.css';
-import ComingSoon from './coming_soon/Index';
-import Header from './header/Index';
-import Footer from './footer/Index';
+import React from "react";
+import "./App.css";
+import { Switch, BrowserRouter, Route } from "react-router-dom";
+import ComingSoon from "./coming_soon/Index";
+import Header from "./header/Index";
+import Footer from "./footer/Index";
+import Privacy from './privacy'
 
 function App() {
   return (
-    <>
-      <Header />
-      <ComingSoon />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Header />
+          <ComingSoon />
+          <Footer />
+        </Route>
+        <Route path="/privacy_policy" exact>
+          <Header />
+          <Privacy />
+          <Footer />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
