06262bb12fef3cd4e29f9bbfe11df2f2d1d21199
Harrison <fayvorharrison101@gmail.com>
2020-12-11 17:38:27 +0100

    design landing page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "./App.css";
+import "./index.css";
 import { Switch, BrowserRouter, Route } from "react-router-dom";
 import ComingSoon from "./coming_soon/Index";
 import Header from "./header/Index";
 import Footer from "./footer/Index";
 import Privacy from './privacy'
+import Home from "./home/Index";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact>
+          <Header />
+          <Home />
+          <Footer />
+        </Route>
+        <Route path="/coming-soon" exact>
           <Header />
           <ComingSoon />
           <Footer />
